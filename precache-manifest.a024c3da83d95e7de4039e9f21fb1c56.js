60a5e094df501ecacb3f82bd01c1fcb8da6c55fa
David Forsberg <david99dimopoulos@gmail.com>
2020-11-17 21:51:55 +0100

    Updates

---
+++ precache-manifest.a024c3da83d95e7de4039e9f21fb1c56.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf14ed8b9be35a3e42a41b6bec753c58",
+    "url": "/anciend/index.html"
+  },
+  {
+    "revision": "dba0fa9afb4f00ec2248",
+    "url": "/anciend/static/css/main.62728c12.chunk.css"
+  },
+  {
+    "revision": "efefd779c505bfac6988",
+    "url": "/anciend/static/js/2.5d9d197d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/anciend/static/js/2.5d9d197d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba0fa9afb4f00ec2248",
+    "url": "/anciend/static/js/main.ba48ac7f.chunk.js"
+  },
+  {
+    "revision": "5f501e73236e0f2838a4",
+    "url": "/anciend/static/js/runtime-main.acd25021.js"
+  },
+  {
+    "revision": "c4acb5961dbe4fefd07eccfd6c3c9e1c",
+    "url": "/anciend/static/media/about_SVG.c4acb596.svg"
+  },
+  {
+    "revision": "01acf875a6387860bae096d51aa72a55",
+    "url": "/anciend/static/media/building.01acf875.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/anciend/static/media/coding.c1a95f73.svg"
+  },
+  {
+    "revision": "8cd40d260755921fd5a48b4e19f24bb3",
+    "url": "/anciend/static/media/completed.8cd40d26.svg"
+  },
+  {
+    "revision": "64fb8a6c031e021a72de8ea970588908",
+    "url": "/anciend/static/media/contact.64fb8a6c.svg"
+  },
+  {
+    "revision": "0a5e21cc51a4475e7291478b285bec8b",
+    "url": "/anciend/static/media/developer.0a5e21cc.png"
+  },
+  {
+    "revision": "e466f68ee8540e70dc8e905874f4f7ff",
+    "url": "/anciend/static/media/developer1.e466f68e.png"
+  },
+  {
+    "revision": "7919b2cbafa7437489cbba9fe1585263",
+    "url": "/anciend/static/media/developer2.7919b2cb.png"
+  },
+  {
+    "revision": "ff1c5c506f8fe387281897cf50da1d0d",
+    "url": "/anciend/static/media/developer3.ff1c5c50.png"
+  },
+  {
+    "revision": "3a25a6f78fe4fc38b46a4692d9ede232",
+    "url": "/anciend/static/media/hero_illustration.3a25a6f7.svg"
+  },
+  {
+    "revision": "e6e8a1903c125b765fcb50e1c411c869",
+    "url": "/anciend/static/media/hero_text.e6e8a190.svg"
+  },
+  {
+    "revision": "22f64e46af91bca2ac26b2525dff8ed0",
+    "url": "/anciend/static/media/man_hr.22f64e46.png"
+  },
+  {
+    "revision": "cd1096f319968c5f19777c9bb90dafff",
+    "url": "/anciend/static/media/man_hr1.cd1096f3.png"
+  },
+  {
+    "revision": "f32f4fecf5b887e7acffaba4ef8e22af",
+    "url": "/anciend/static/media/man_hr2.f32f4fec.png"
+  },
+  {
+    "revision": "26e52cd1b2f27b936d9c79e0c14ef63c",
+    "url": "/anciend/static/media/planning.26e52cd1.svg"
+  },
+  {
+    "revision": "24313980d0737d2323fa58dfff9a89f1",
+    "url": "/anciend/static/media/woman_hr.24313980.png"
+  },
+  {
+    "revision": "d5b2ee1c2a9c77a359431089a52ea9fa",
+    "url": "/anciend/static/media/woman_hr1.d5b2ee1c.png"
+  },
+  {
+    "revision": "d456f4fb935e7c427b885c9825725436",
+    "url": "/anciend/static/media/woman_owner.d456f4fb.png"
+  }
+]);
